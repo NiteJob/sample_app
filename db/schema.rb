a75a8f6388bd1dc9a12148bda6a6ae7c853eb726
Infojedi <infojedi@comcast.net>
2013-08-20 00:19:14 -0400

    Dont run db test:prepare - Specify RAILS ENV=test

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130814050118) do
+ActiveRecord::Schema.define(version: 20130820033341) do
 
   create_table "static_pgs", force: true do |t|
     t.string   "pgname"
     t.text     "pgtxt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
