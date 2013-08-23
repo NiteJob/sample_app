1cd793466dc518370ac09b5ba1178417db4f6d2c
Infojedi <infojedi@comcast.net>
2013-08-23 19:42:49 -0400

    Fix Postgresql DB problem - Done - Should be back on track

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130821041923) do
+ActiveRecord::Schema.define(version: 20130823231634) do
 
   create_table "static_pgs", force: true do |t|
     t.string   "pgname"
