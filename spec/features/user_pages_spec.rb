a51727113c6d61adcd71ceddef149643e515ace9
Infojedi <infojedi@comcast.net>
2013-08-26 22:09:30 -0400

    Chp9 Ex1 completed - Done ok

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "User pages" do
+describe "User pages", type: :request do
 
   subject { page }
 
@@ -147,6 +147,16 @@ describe "User pages" do
       it { should have_link('Sign out', href: signout_path) }
       specify { expect(user.reload.name).to  eq new_name }
       specify { expect(user.reload.email).to eq new_email }
+    end
+
+    describe "forbidden attributes" do
+      let(:params) do
+        { user: { admin: true, password: user.password,
+                  password_confirmation: user.password } }
+      end
+      before { sign_in user, no_capybara: true
+                patch user_path(user), params }
+      specify { expect(user.reload).not_to be_admin }
     end
 
   end
